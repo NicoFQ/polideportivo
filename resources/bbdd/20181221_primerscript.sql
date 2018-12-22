df666f96ad69cedc33d9a5d6eb8046595729d2e7
NicoFQ-Casa <nicolasmatiasshm@hotmail.es>
2018-12-22 22:59:38 +0100

    Base de datos estable
     - Se han probado las tablas con datos y funciona correctamente
     - Pendiente meter datos a tablas clase y asiste.

--- resources/bbdd/20181221_primerscript.sql
+++ resources/bbdd/20181221_primerscript.sql
@@ -31,11 +31,12 @@ PRIMARY KEY (id_deporte)
 CREATE TABLE pista
 (
  id_pista       varchar(20) NOT NULL ,
- n_pista        varchar(20) NOT NULL ,
+ n_pista        varchar(100) NOT NULL ,
  precio_hora    double NOT NULL ,
  id_deporte     varchar(45) NOT NULL ,
  id_instalacion varchar(45) NOT NULL ,
  estado         int NOT NULL ,
+ comentarios    varchar(500) NULL ,
 PRIMARY KEY (id_pista),
 KEY fkIdx_374 (id_deporte),
 CONSTRAINT FK_374 FOREIGN KEY fkIdx_374 (id_deporte) REFERENCES deporte (id_deporte) ON DELETE CASCADE,
@@ -69,77 +70,97 @@ PRIMARY KEY (id_tipo_usuario)
 
 CREATE TABLE usuario
 (
- id_usuario       varchar(45) NOT NULL ,
+ id_usuario       INT AUTO_INCREMENT NOT NULL ,
  email            varchar(45) NOT NULL ,
+ contrasena       varchar(45) NOT NULL ,
  dni              varchar(9) NOT NULL ,
  nombre           varchar(45) NOT NULL ,
  apellido_1       varchar(45) NOT NULL ,
  apellido_2       varchar(45) ,
  nombre_usuario   varchar(45) NOT NULL ,
- contrasena       varchar(45) NOT NULL ,
- fecha_alta       date NOT NULL ,
  fecha_nacimiento date NOT NULL ,
  sexo             int ,
  nacionalidad     varchar(45) ,
  id_tipo_usuario  varchar(45) NOT NULL ,
-PRIMARY KEY (email, id_usuario),
+ fecha_alta       TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+PRIMARY KEY (id_usuario),
 KEY fkIdx_384 (id_tipo_usuario),
 CONSTRAINT FK_384 FOREIGN KEY fkIdx_384 (id_tipo_usuario) REFERENCES tipo_usuario (id_tipo_usuario) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
 -- ************************************** reserva
 
+-- ****************** SqlDBM: MySQL ******************;
+-- ***************************************************;
+
+
+-- ************************************** reserva
+
 CREATE TABLE reserva
 (
- id_reserva     int NOT NULL ,
- id_usuario     varchar(45) NOT NULL ,
+ id_reserva     varchar(45) NOT NULL ,
+ id_usuario     int NOT NULL ,
  fecha          date NOT NULL ,
  hora_inicio    time NOT NULL ,
  hora_fin       time NOT NULL ,
  id_pista       varchar(20) NOT NULL ,
- email          varchar(45) NOT NULL ,
  precio_reserva double ,
-PRIMARY KEY (email, id_reserva, id_usuario, fecha, hora_inicio, hora_fin, id_pista),
-KEY fkIdx_277 (email, id_usuario),
-CONSTRAINT FK_277 FOREIGN KEY fkIdx_277 (email, id_usuario) REFERENCES usuario (email, id_usuario) ON DELETE CASCADE,
+PRIMARY KEY (id_reserva, id_usuario, fecha, hora_inicio, hora_fin, id_pista),
+KEY fkIdx_277 (id_usuario),
+CONSTRAINT FK_277 FOREIGN KEY fkIdx_277 (id_usuario) REFERENCES usuario (id_usuario),
 KEY fkIdx_330 (hora_inicio, hora_fin, id_pista, fecha),
 CONSTRAINT FK_330 FOREIGN KEY fkIdx_330 (hora_inicio, hora_fin, id_pista, fecha) REFERENCES horario (hora_inicio, hora_fin, id_pista, fecha) ON DELETE CASCADE
 ) ENGINE=INNODB;
+
+
+
+
+-- ****************** SqlDBM: MySQL ******************;
+-- ***************************************************;
+
 
 -- ************************************** clase
 
 CREATE TABLE clase
 (
- id_clase     varchar(20) NOT NULL ,
+ cd_clase     varchar(20) NOT NULL ,
  fecha        date NOT NULL ,
  hora_inicio  time NOT NULL ,
  hora_fin     time NOT NULL ,
  id_pista     varchar(20) NOT NULL ,
- nombre_clase varchar(45) NOT NULL ,
- precio_clase double NOT NULL ,
- id_usuario   varchar(45) NOT NULL ,
- email        varchar(45) NOT NULL ,
-PRIMARY KEY (id_clase, fecha, hora_inicio, hora_fin, id_pista),
+ combre_clase varchar(45) NOT NULL ,
+ crecio_clase double NOT NULL ,
+ id_usuario   int NOT NULL ,
+PRIMARY KEY (cd_clase, fecha, hora_inicio, hora_fin, id_pista),
 KEY fkIdx_349 (hora_inicio, hora_fin, id_pista, fecha),
 CONSTRAINT FK_349 FOREIGN KEY fkIdx_349 (hora_inicio, hora_fin, id_pista, fecha) REFERENCES horario (hora_inicio, hora_fin, id_pista, fecha) ON DELETE CASCADE,
-KEY fkIdx_380 (email, id_usuario),
-CONSTRAINT FK_380 FOREIGN KEY fkIdx_380 (email, id_usuario) REFERENCES usuario (email, id_usuario) ON DELETE CASCADE
+KEY fkIdx_380 (id_usuario),
+CONSTRAINT FK_380 FOREIGN KEY fkIdx_380 (id_usuario) REFERENCES usuario (id_usuario) ON DELETE CASCADE
 ) ENGINE=INNODB;
+
+
+
+
+
 
 -- ************************************** asiste
 
 CREATE TABLE asiste
 (
- id_usuario  varchar(45) NOT NULL ,
- id_clase    varchar(20) NOT NULL ,
+ id_usuario  int NOT NULL ,
+ cd_clase    varchar(20) NOT NULL ,
  fecha       date NOT NULL ,
  hora_inicio time NOT NULL ,
  hora_fin    time NOT NULL ,
  id_pista    varchar(20) NOT NULL ,
- email       varchar(45) NOT NULL ,
-PRIMARY KEY (email, id_usuario, id_clase, fecha, hora_inicio, hora_fin, id_pista),
-KEY fkIdx_338 (email, id_usuario),
-CONSTRAINT FK_338 FOREIGN KEY fkIdx_338 (email, id_usuario) REFERENCES usuario (email, id_usuario) ON DELETE CASCADE,
-KEY fkIdx_343 (id_clase, fecha, hora_inicio, hora_fin, id_pista),
-CONSTRAINT FK_343 FOREIGN KEY fkIdx_343 (id_clase, fecha, hora_inicio, hora_fin, id_pista) REFERENCES clase (id_clase, fecha, hora_inicio, hora_fin, id_pista) ON DELETE CASCADE
+PRIMARY KEY (id_usuario, cd_clase, fecha, hora_inicio, hora_fin, id_pista),
+KEY fkIdx_338 (id_usuario),
+CONSTRAINT FK_338 FOREIGN KEY fkIdx_338 (id_usuario) REFERENCES usuario (id_usuario) ON DELETE CASCADE,
+KEY fkIdx_343 (cd_clase, fecha, hora_inicio, hora_fin, id_pista),
+CONSTRAINT FK_343 FOREIGN KEY fkIdx_343 (cd_clase, fecha, hora_inicio, hora_fin, id_pista) REFERENCES clase (cd_clase, fecha, hora_inicio, hora_fin, id_pista) ON DELETE CASCADE
 ) ENGINE=INNODB;
+
+
+
+
+
---
+++ resources/bbdd/datos-polideportivo.sql
@@ -0,0 +1,289 @@
+insert into instalacion (id_instalacion,nombre_insta)
+values ("INST-01", "Instalacion de Baloncesto");
+
+insert into instalacion (id_instalacion,nombre_insta)
+values ("INST-02", "Instalacion de Padel");
+
+insert into instalacion (id_instalacion,nombre_insta)
+values ("INST-03", "Instalacion de Futbol");
+
+insert into instalacion (id_instalacion,nombre_insta)
+values ("INST-04", "Instalacion de Futbol");
+
+
+
+insert into deporte (id_deporte,nombre_deporte)
+values ("DEP-FUTBOL", "Futbol");
+
+insert into deporte (id_deporte,nombre_deporte)
+values ("DEP-BALONCESTO", "Baloncesto");
+
+insert into deporte (id_deporte,nombre_deporte)
+values ("DEP-PADEL", "Padel");
+
+insert into deporte (id_deporte,nombre_deporte)
+values ("DEP-TENIS", "Tenis");
+
+insert into deporte (id_deporte,nombre_deporte)
+values ("DEP-VOLEY", "Voleybol");
+
+insert into deporte (id_deporte,nombre_deporte)
+values ("DEP-NAT", "Natacion");
+
+
+
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-01", "Pista de Futbol", 3.5,"DEP-FUTBOL", "INST-01", 1);
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-02", "Pista de Futbol", 3.5,"DEP-FUTBOL", "INST-01", 1);
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-03", "Pista de Baloncesto", 3.5,"DEP-BALONCESTO", "INST-02", 1);
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-04", "Pista de Baloncesto", 3.5,"DEP-BALONCESTO", "INST-02", 1);
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-05", "Pista de Voleybol", 3.5,"DEP-VOLEY", "INST-03", 1);
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-06", "Pista de Voleybol", 3.5,"DEP-VOLEY", "INST-03", 1);
+
+insert into pista (id_pista,n_pista,precio_hora,id_deporte,id_instalacion,estado)
+values ("P-07", "Pista de Natacion", 3.5,"DEP-NAT", "INST-04", 1);
+
+
+
+
+insert into horario (fecha,hora_inicio,hora_fin,id_pista)
+values ("2018-12-23", "15:00:00", "15:59:59", "P-01");
+
+insert into horario (fecha,hora_inicio,hora_fin,id_pista)
+values ("2018-12-23", "16:00:00", "16:59:59", "P-01");
+
+insert into horario (fecha,hora_inicio,hora_fin,id_pista)
+values ("2018-12-23", "15:00:00", "16:00:00", "P-02");
+
+insert into horario (fecha,hora_inicio,hora_fin,id_pista)
+values ("2018-12-23", "15:00:00", "16:00:00", "P-03");
+
+insert into horario (fecha,hora_inicio,hora_fin,id_pista)
+values ("2018-12-23", "15:00:00", "16:00:00", "P-04");
+
+insert into horario (fecha,hora_inicio,hora_fin,id_pista)
+values ("2018-12-23", "15:00:00", "16:00:00", "P-05");
+
+
+
+
+insert into tipo_usuario (id_tipo_usuario,nombre_tipo)
+values ("AD", "Administrador");
+insert into tipo_usuario (id_tipo_usuario,nombre_tipo)
+values ("CL", "Cliente");
+insert into tipo_usuario (id_tipo_usuario,nombre_tipo)
+values ("PR", "Profesor");
+
+
+
+insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"nicolas@hotmail.es",
+	"1234",
+	"12345678P",
+	"Nicolas",
+	"Avila",
+	"Lopez",
+	"Nico123",
+	"1996-03-30",
+	"0",
+	"Boliviana",
+	"AD");
+
+
+insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"kevin@hotmail.es",
+	"1234",
+	"12345678P",
+	"Kevin",
+	"Avila",
+	"Lopez",
+	"Kevin123",
+	"1996-03-30",
+	"0",
+	"Boliviana",
+	"AD");
+
+	insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"miguel@hotmail.es",
+	"1234",
+	"12345678P",
+	"Miguel",
+	"Avila",
+	"Lopez",
+	"Miguel123",
+	"1996-03-30",
+	"0",
+	"Española",
+	"AD");
+
+
+	insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"mario@hotmail.es",
+	"1234",
+	"12345678P",
+	"Mario",
+	"Avila",
+	"Lopez",
+	"Mario123",
+	"1996-03-30",
+	"0",
+	"Española",
+	"AD");
+
+	
+insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"sergio@hotmail.es",
+	"1234",
+	"12345678P",
+	"Sergio",
+	"Avila",
+	"Lopez",
+	"Sergio123",
+	"1996-03-30",
+	"0",
+	"Española",
+	"CL");
+
+
+insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"victor@hotmail.es",
+	"1234",
+	"12345678P",
+	"Victor",
+	"Avila",
+	"Lopez",
+	"Victor123",
+	"1996-03-30",
+	"0",
+	"Española",
+	"CL");
+
+	
+insert into usuario (
+	email,
+	contrasena,
+	dni,
+	nombre,
+	apellido_1,
+	apellido_2,
+	nombre_usuario,
+	fecha_nacimiento,
+	sexo,
+	nacionalidad,
+	id_tipo_usuario
+	) values (
+	"alvaro@hotmail.es",
+	"1234",
+	"12345678P",
+	"Alvaro",
+	"Avila",
+	"Lopez",
+	"Alvaro123",
+	"1996-03-30",
+	"0",
+	"Española",
+	"PR");
+
+	
+	insert into reserva (
+	id_reserva,
+	id_usuario,
+	fecha,
+	hora_inicio,
+	hora_fin,
+	id_pista,
+	precio_reserva
+	) values (
+	"123456789",
+	6,
+	"2018-12-23",
+	"15:00:00",
+	"15:59:59",
+	"P-01",
+	5.69
+	);
+
+	
