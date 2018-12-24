3f5d0a6c4d58e9f286d692ec9ec317938a023f9f
Kevin <kevin@gmail.com>
2018-12-24 18:41:11 +0100

    Footer completado solo para Desktop -Pendiente de hacerlo responsive

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php 
-require('../src/registro/registro.php');
-
-
+// REQUIRES
+require('../src/comunes_polideportivo/footer.php')
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="./css/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <title>Home</title>
+</head>
+<body>
+    <?= footer()?>
+</body>
+</html>
