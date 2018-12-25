b014db6d20332ef9b819cba9614db984611b39f8
NicoFQ-Casa <nicolasmatiasshm@hotmail.es>
2018-12-25 18:30:57 +0100

    Login estable
     - Pendiente implementar sesiones.

---
+++ src/autentificacion.php
@@ -0,0 +1,29 @@
+<?php 
+	spl_autoload_register(function ($class) {
+    $classPath = "./../src/";
+    require("$classPath${class}.php");
+	});
+
+	/**
+	* Funcion que autentificará usuarios 
+	* contra la base de datos. Devolvera verdadero
+	* en caso de que el usuario y contrasa sea correcto
+	* false en caso contrario.
+	*
+	* @param $user Indica el email o nomre de usuario.
+	* @param $pass Indica la contraseña.
+	**/
+	
+	function autentificar(string $user, string $pass){
+		$encontrado = false;
+		if ($usuario = Usuario::existeUsuario($user)) {
+			if (password_verify($pass, $usuario[0]['contrasena'])) {
+				$encontrado = true;
+				$usuario = new Usuario($usuario[0]);
+				$usuario->homeUsuario();
+			}
+		}
+		return $encontrado;
+	}
+	
+ ?>
