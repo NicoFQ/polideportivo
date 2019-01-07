699068ce201bd22bd90a6be340c8dbbddeca433c
NicoFQ-Casa <nicolasmatiasshm@hotmail.es>
2019-01-07 21:39:15 +0100

    Se añaden registros a la base de datos.
     - Se incrementan los usuarios.

--- bin/despliegue-bbdd-polideportivo.sh
+++ bin/despliegue-bbdd-polideportivo.sh
@@ -3,8 +3,8 @@ BDUsuario="admin_polideportivo"
 BDNombreBD="proyecto_polideportivo"
 BDContrasena="1234"
 
-BDCreaTablas="resources/bbdd/tablas-polideportivo.sql"
-BDCreaDatos="resources/bbdd/datos-polideportivo.sql"
+BDCreaTablas="./resources/bbdd/tablas-polideportivo.sql"
+BDCreaDatos="./resources/bbdd/datos-polideportivo.sql"
 
 mysql -u $BDUsuario -p$BDContrasena $BDNombreBD < $BDCreaTablas
 mysql -u $BDUsuario -p$BDContrasena $BDNombreBD < $BDCreaDatos
