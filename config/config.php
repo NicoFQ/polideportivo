1c4932f15c9ce2ccc6de53170ef47e43dc869089
NicoFQ-Casa <nicolasmatiasshm@hotmail.es>
2019-03-01 21:01:20 +0100

    Limpieza de archivos del polideportivo.
     - Cambiando a Symfony.

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// **Comprobar necesidad de implementacion de esta funcion**
-
-spl_autoload_register(function ($class) {
-    $classPath = "./../src/";
-    require("$classPath${class}.php");
-});
-?>
