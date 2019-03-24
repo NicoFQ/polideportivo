6b72deafdd0f582a94ab11d57cb6156c0453213e
Kevin <kevinmb41@hotmail.com>
2019-03-24 18:35:58 +0100

    Solucionado pequeño problema del despliegue de la BBDD

--- resources/bbdd/despliegue.sql
+++ resources/bbdd/despliegue.sql
@@ -2,5 +2,5 @@ CREATE USER IF NOT EXISTS 'admin_polideportivo'@'localhost' IDENTIFIED BY '1234'
 
 CREATE DATABASE IF NOT EXISTS proyecto_polideportivo;
 
-GRANT ALL PRIVILEGES ON admin_polideportivo.* TO 'admin_polideportivo'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON proyecto_polideportivo.* TO 'admin_polideportivo'@'localhost' WITH GRANT OPTION;
 
