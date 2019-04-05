1233c7bc4f4dc3dce85587e6aa01b866a1eb50ab
Kevin <Kevinmb41@hotmail.com>
2019-04-05 19:35:37 +0200

    Db TipoPago

---
+++ src/Entity/TipoPago.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TipoPagoRepository")
+ */
+class TipoPago
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre_tipo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombreTipo(): ?string
+    {
+        return $this->nombre_tipo;
+    }
+
+    public function setNombreTipo(string $nombre_tipo): self
+    {
+        $this->nombre_tipo = $nombre_tipo;
+
+        return $this;
+    }
+}
